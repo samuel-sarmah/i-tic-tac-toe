125061992c93544ba28c4dcb704dc76e8928c9aa
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-02-04 07:43:08 +0200

    feat: add winning patters

--- script.js
+++ script.js
@@ -15,4 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let playerMode = modeSelector.ariaValueMax; // Default: Player vs Player
     let playerXScore = 0;
     let playerOScore = 0;
+
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
 })
